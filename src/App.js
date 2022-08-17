37535356519360b5fffc888a10e7a67a56adf87d
khampheng-ctv <khamphengchanthavong@gmail.com>
2022-08-17 15:44:04 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:4001";
 
 function App() {
+  const [response, setResponse] = useState([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    const fetchData = async () => {
+      const res = await fetch(`${ENDPOINT}/get`);
+      const data = await res.json();
+      setResponse(data);
+    };
+
+    //socket.io
+    socket.on("getMessage", (data) => {
+      if (data) {
+        setResponse(data);
+      }
+    });
+
+    //fetch
+    fetchData();
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const addMessage = async () => {
+    try {
+      await fetch(`${ENDPOINT}/add`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: message }),
+      });
+      setMessage("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-5">
+      <div className="text-end">
+        {response?.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
+      <div className="text-end">
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control shadow-none"
+            placeholder="text..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                addMessage();
+              }
+            }}
+          />
+          <button
+            className="btn btn-primary shadow-none"
+            disabled={!message}
+            onClick={addMessage}
+          >
+            Send
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
